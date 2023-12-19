8cc0b2390d52b39526b62e569e39b278eb132a3b
Jan Zelinka <jan.zelinka12@gmail.com>
2023-12-19 19:33:47 +0100

    node version, webpack config to typescript

---
+++ app-react/webpack.config.ts
@@ -0,0 +1,42 @@
+import path from 'path';
+import ESLintPlugin from 'eslint-webpack-plugin';
+
+const config = {
+  plugins: [
+    new ESLintPlugin({
+      extensions: ['ts', 'tsx'],
+      files: ['src/**/*.ts', 'src/**/*.tsx'],
+    }),
+  ],
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 3000, // You can change the port number if needed
+    hot: true,
+    proxy: {
+      '/Login': 'http://localhost:5262',
+      '/WeatherForecast': 'http://localhost:5262',
+    },
+  },
+};
+
+export default config;
