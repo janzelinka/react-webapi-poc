f9565fabc7e9608f83ec468e3136ff69532b2c87
zeLo <jan.zelinka12@gmail.com>
2025-03-27 10:41:55 -0400

    theninclude to join all related entities

--- app-api/app-api/Controllers/EnumController.cs
+++ app-api/app-api/Controllers/EnumController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authentication;
 using api.Repositories;
 using api.Models.Events;
+using Microsoft.EntityFrameworkCore;
 
 namespace ing_app_api.Controllers
 {
@@ -13,36 +14,43 @@ namespace ing_app_api.Controllers
         public EnumController(
             ICityRepository cityRepository, 
             ICountryRepository countryRepo,
-            IDistrictRepository districtRepository)
+            IDistrictRepository districtRepository,
+            ApiDataContext ctx
+        )
         {
             CityRepository = cityRepository;
             CountryRepo = countryRepo;
             DistrictRepository = districtRepository;
+            Ctx = ctx;
         }
 
         public ICityRepository CityRepository { get; }
         public ICountryRepository CountryRepo { get; }
         public IDistrictRepository DistrictRepository { get; }
+        public ApiDataContext Ctx { get; }
 
         [HttpGet]
         [Route("[controller]/GetAllCities")]
-        public IEnumerable<City> GetAllCities()
+        public IEnumerable<City> GetAllCities(int page = 0, int pageSize = 20)
         {
-            return CityRepository.GetAll();
+            return CityRepository.GetAll(c => c.District).Skip(page * pageSize).Take(pageSize).ToList();
         }
 
         [HttpGet]
         [Route("[controller]/GetAllCountries")]
         public IEnumerable<Country> GetAllCountries()
         {
-            return CountryRepo.GetAll();
+            return Ctx.Countries
+            .Include(country => country.Regions)
+            .ThenInclude(region => region.Districts)
+            .ThenInclude(district => district.Cities);
         }
 
         [HttpGet]
         [Route("[controller]/GetAllDistricts")]
         public IEnumerable<District> GetAllDistricts()
         {
-            return DistrictRepository.GetAll();
+            return DistrictRepository.GetAll(c => c.Cities);
         }
 
 
